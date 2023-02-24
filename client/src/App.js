263d2deb651b6acdba7e559de0b2a7326c5442c7
Abdul Majid <fa19-bse-014@cuilahore.edu.pk>
2023-02-24 10:44:34 +0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+     <h1>Welcome to Materializer</h1>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -7,9 +7,6 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
